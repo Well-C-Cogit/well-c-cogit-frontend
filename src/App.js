41a9dd731c10496b140da9ac766518764b1d264c
seonkyo <tjs0466@naver.com>
2024-09-05 17:10:16 +0900

    feat: add router basename

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import MyHeader from './components/MyHeader';
-import Communities from './components/Communities';
-import NavigationBar from './components/NavigationBar';
-import DetailPage from './components/DetailPage';
-import ProfilePage from './components/ProfilePage';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import MyHeader from "./components/MyHeader";
+import Communities from "./components/Communities";
+import NavigationBar from "./components/NavigationBar";
+import DetailPage from "./components/DetailPage";
+import ProfilePage from "./components/ProfilePage";
+import "./App.css";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/">
       <div className="app">
         <NavigationBar />
         <div className="scrollable-content">
           <Routes>
-            <Route path="/" element={
-              <div>
-                <h2 className="my-profile-title">내 정보</h2>
-                <div className="my-info">
-                  <MyHeader />
-                  <Communities />
+            <Route
+              path="/"
+              element={
+                <div>
+                  <h2 className="my-profile-title">내 정보</h2>
+                  <div className="my-info">
+                    <MyHeader />
+                    <Communities />
+                  </div>
                 </div>
-              </div>
-            } />
+              }
+            />
             <Route path="/detail" element={<DetailPage />} />
             <Route path="/profile" element={<ProfilePage />} />
           </Routes>
